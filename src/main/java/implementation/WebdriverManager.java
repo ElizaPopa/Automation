9d9a619dcb448e2a8642783e1673274664b5dc65
ElizaPopa <oprea.eliza7@gmail.com>
2022-10-28 15:44:30 +0300

    Grid update

--- src/main/java/implementation/WebdriverManager.java
+++ src/main/java/implementation/WebdriverManager.java
@@ -3,10 +3,18 @@ package implementation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WebdriverManager {
+
+    public static RemoteWebDriver getRemoteWebDriver() throws MalformedURLException {
+        return new RemoteWebDriver(new URL("http://localhost:4444/"), new ChromeOptions());
+    }
 
     public static ChromeDriver getChromedriver() {
         WebDriverManager.chromedriver().setup();
